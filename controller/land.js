487d7d199d71d9e1dc8aab49e7c06df0ce3c9545
doohwa <doohwa@xenix.net>
2014-09-07 14:02:46 +0900

    정복 창 소비 추가08

--- controller/land.js
+++ controller/land.js
@@ -80,7 +80,8 @@ land = {
 								var startDate = moment(resArray.spearUpdateDate).zone(-9);
 								var endDate = moment();
 								
-								var secondsDiff = endDate.diff(startDate, 'seconds');
+								//var secondsDiff = endDate.diff(startDate, 'seconds');
+								var secondsDiff = startDate.diff(endDate, 'seconds');
 								console.log(startDate);
 								console.log(endDate);
 								console.log(secondsDiff);
